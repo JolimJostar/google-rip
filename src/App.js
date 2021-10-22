41e635004c893cbfeb949254eabcd855e75ab571
Yan Kochkin <ssffggdd@mail.ru>
2021-10-22 15:08:39 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Test react app
+      </h1>
+      <h2>
+        TEst react app but h2
+      </h2>
     </div>
   );
 }
