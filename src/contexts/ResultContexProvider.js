a4a7ee55f41331084e87fa60dd5076a739b8cab0
Yan Kochkin <ssffggdd@mail.ru>
2021-10-25 13:22:48 +0300

    Added Basic Api

---
+++ src/contexts/ResultContexProvider.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
+
+export const ResultContexProvider = ( { children } ) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const getResults = async (type) => {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': '23f6545f22msh416ddfe9cd9a41ep1cbff4jsn07628abf7be6'
+            }
+        })
+
+        const data = await response.json();
+
+        setResults(data)
+        setIsLoading(false)
+    }
+
+}
