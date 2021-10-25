4751f899f48814000647c3456a156eed791d7632
Yan Kochkin <ssffggdd@mail.ru>
2021-10-25 22:04:08 +0300

    Now api results show

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
 
-export const ResultContexProvider = ( { children } ) => {
+export const ResultContextProvider = ( { children } ) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
@@ -26,4 +26,12 @@ export const ResultContexProvider = ( { children } ) => {
         setIsLoading(false)
     }
 
-}
+    return (
+        <ResultContext.Provider value={ { getResults, results, searchTerm, setSearchTerm, isLoading } } >
+            { children }
+        </ResultContext.Provider>
+    )
+
+}; 
+
+export const useResultContext = () => useContext(ResultContext);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { ResultContextProvider } from './contexts/ResultContextProvider'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>,
-  </React.StrictMode>,
+  <ResultContextProvider >
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>,
+    </React.StrictMode>
+  </ResultContextProvider>,
   document.getElementById('root')
 );
 
