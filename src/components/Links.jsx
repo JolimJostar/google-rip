434cdfd75f32c5b78792ef8de0c71649766b358a
Yan Kochkin <ssffggdd@mail.ru>
2021-10-27 01:28:14 +0300

    got damn it, idk why, but getting rid of if statments in setResults(data) fixes the bug of empty array appearing

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,7 +12,11 @@ const links = [
 export const Links = () => {
     return (
         <div className='flex justify-between items-center mt-4 sm:justify-between'>
-            Links
+            {links.map(( { url, text } ) => (
+                <NavLink to={url} activeClassName='text-blue-700 border-b-2 border-blue-700 pb-2 dark:text-blue-300' >
+                    {text}
+                </NavLink>
+            ))}
         </div>
     )
 }
